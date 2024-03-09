54518b2cae6d07f301176973fbaeee73bc41e821
Russel-coder-ui <155598461+Russel-coder-ui@users.noreply.github.com>
2024-03-09 22:30:09 +0800

    login_register

--- frontend/src/Screens/LoginScreen.jsx
+++ frontend/src/Screens/LoginScreen.jsx
@@ -6,6 +6,7 @@ import Loader from '../Components/Loader';
 import Message from '../Components/Message';
 import { login } from '../actions/userActions';
 import FormContainer from '../Components/FormContainer';
+import './register.css';
 
 function LoginScreen() {
     const [email, setEmail] = useState('');
@@ -34,46 +35,62 @@ function LoginScreen() {
     }
 
     return (
-        <FormContainer>
-            <h1>Sign In</h1>
-            {error && <Message variant='danger'>{error}</Message>}
-            {loading && <Loader />}
-            <Form onSubmit={submitHandler}>
-                <Form.Group controlId='email'>
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder='Enter email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </Form.Group>
-
-                <Form.Group controlId='password'>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type='password'
-                        placeholder='Enter password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </Form.Group>
-
-                <Button type='submit' variant='primary'>
-                    Sign In
-                </Button>
-            </Form>
-            <Row className='py-3'>
-                <Col>
-                    <Link to='/register'>Register</Link>
+        <div className="inputContainer">
+            
+           
+            <Row>
+                <Col className='video' md={6}>
+                    <video src="/Images/Gifforcode.mp4" autoPlay muted loop></video>
                 </Col>
-                <Col>
-                    <Button variant='link' onClick={handleRequestPasswordChange}>
-                        Forgot password?
-                    </Button>
+                <Col md={6} style={{ paddingTop: '20px' }}>
+                    <FormContainer>
+                        <h1>Sign In</h1>
+                        <p className='ms-60'>WELCOME BACK!</p>
+                        {error && <Message variant='danger'>{error}</Message>}
+                        {loading && <Loader />}
+                        <Form onSubmit={submitHandler}>
+                            <div className='userInputContainer'>
+                                <Form.Group controlId='email'>
+                                    <Form.Label>Email Address</Form.Label>
+                                    <Form.Control
+                                        type='text'
+                                        placeholder='Enter email'
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </Form.Group>
+                            </div>
+
+                            <div className='userInputContainer'>
+                                <Form.Group controlId='password'>
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                        type='password'
+                                        placeholder='Enter password'
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
+                                </Form.Group>
+                            </div>
+
+                            <Button type='submit' variant='primary'>
+                                Sign In
+                            </Button>
+                        </Form>
+                        <Row className='py-3'>
+                            <Col>
+                                <Link to='/register'>Register</Link>
+                            </Col>
+                            <Col>
+                                <Button variant='link' onClick={handleRequestPasswordChange}>
+                                    Forgot password?
+                                </Button>
+                            </Col>
+                        </Row>
+                    </FormContainer>
                 </Col>
             </Row>
-        </FormContainer>
+        </div>
     );
 }
 
--- frontend/src/Screens/RegisterScreen.jsx
+++ frontend/src/Screens/RegisterScreen.jsx
@@ -4,6 +4,7 @@ import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { register } from '../actions/userActions'; // Import the register action
 import FormContainer from '../Components/FormContainer';
+import './register.css'; //Import the CSS file
 
 function RegisterScreen() {
   const [name, setName] = useState('');
@@ -16,11 +17,7 @@ function RegisterScreen() {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    // console.log('Name:', name);
-    // console.log('Email:', email);
-    // console.log('Password:', password);
-    // console.log('User Type:', userType);
-    // console.log('Confirm Password:', confirmPassword); // pang test
+    
   
     if (password !== confirmPassword) {
       alert('Passwords do not match');
@@ -32,67 +29,89 @@ function RegisterScreen() {
   
 
   return (
-    <FormContainer>
-      <h1>Sign Up</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group controlId='name'>
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Enter your name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId='email'>
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Enter email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId='password'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Enter password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId='confirmPassword'>
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Confirm password'
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId='userType'>
-          <Form.Label>User Type</Form.Label>
-          <Form.Control
-            as='select'
-            value={userType}
-            onChange={(e) => setUserType(e.target.value)} // Update userType state
-          >
-            <option value='student'>Student</option>
-            <option value='instructor'>Instructor</option>
-          </Form.Control>
-        </Form.Group>
-        <Button type='submit' variant='primary'>
-          Register
-        </Button>
-      </Form>
-      <Row className='py-3'>
-        <Col>
-          Already registered? <Link to='/login'>Sign In</Link>
+    <div className="inputContainer">
+      <Row>
+        <Col className='video' md={6} style={{ position: 'relative' }}>
+          <video src="/Images/Gifforcode.mp4" autoPlay muted loop></video>
+          
+          <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'rgba(255, 255, 255, 0.5)', padding: '10px', borderRadius: '5px' }}>
+            <p style={{ margin: 0 }}>REVIL: Resources for Video Learning</p>
+          </div>
+        </Col>
+        <Col md={6}>
+          <h1>Register</h1>
+          <Form onSubmit={submitHandler}>
+            <div className='userInputContainer'>
+              <Form.Group controlId='name'>
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Enter your name'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </Form.Group>
+            </div>
+            <div className='userInputContainer'>
+              <Form.Group controlId='email'>
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  type='email'
+                  placeholder='Enter email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+            </div>
+            <div className='userInputContainer'>
+              <Form.Group controlId='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type='password'
+                  placeholder='Enter password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+            </div>
+            <div className='userInputContainer'>
+              <Form.Group controlId='confirmPassword'>
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type='password'
+                  placeholder='Confirm password'
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+              </Form.Group>
+            </div>
+            <div className='userInputContainer'>
+              <Form.Group controlId='userType'>
+                <Form.Label>User Type</Form.Label>
+                <Form.Control
+                  as='select'
+                  value={userType}
+                  onChange={(e) => setUserType(e.target.value)}
+                >
+                  <option value='student'>Student</option>
+                  <option value='instructor'>Instructor</option>
+                </Form.Control>
+              </Form.Group>
+            </div>
+            <Button type='submit' variant='primary'>
+              Register
+            </Button>
+          </Form>
+          <Row className='py-3'>
+            <Col>
+              Already registered? <Link to='/login'>Sign In</Link>
+            </Col>
+          </Row>
         </Col>
       </Row>
-    </FormContainer>
+    </div>
   );
 }
 
 export default RegisterScreen;
+
